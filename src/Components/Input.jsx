e5dacf2fc86ba342bef97baad601b49ff75653c0
Masencod <126698845+Masencod@users.noreply.github.com>
2023-07-04 21:02:04 +0330

    fixed inputs

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -32,9 +32,9 @@ const Input = ({ onSubmit,list }) => {
     }
   return (
     <form onSubmit={handleSubmit} className="p-5 flex flex-col sm:flex-row sm:justify-start gap-2 w-full">
-        <input value={name} onChange={({ target }) => {setName(target.value)}} className={`outline-none rounded-md p-2 sm:w-3/12 border-2 ${nameError ? 'border-red-500' : 'border-fuchsia-400'}`} placeholder="Your name"  type="text" />
-        <input value={phone} onChange={({ target }) => {setPhone(target.value)}} className={`outline-none rounded-md p-2 sm:w-3/12 border-2 ${phoneError ? 'border-red-500' : 'border-fuchsia-400'}`} placeholder="Your number" type="text" />
-        <input value={email} onChange={({ target }) => {setEmail(target.value)}} className={`outline-none rounded-md p-2 sm:w-3/12 border-2 ${emailError ? 'border-red-500' : 'border-fuchsia-400'}`} placeholder="Your email" type="text" />
+        <input value={name} onChange={({ target }) => {setName(target.value)}} className={`bg-black outline-none rounded-md p-2 sm:w-3/12 border-2 ${nameError ? 'border-red-500' : 'border-fuchsia-400'}`} placeholder="Your name"  type="text" />
+        <input value={phone} onChange={({ target }) => {setPhone(target.value)}} className={`bg-black outline-none rounded-md p-2 sm:w-3/12 border-2 ${phoneError ? 'border-red-500' : 'border-fuchsia-400'}`} placeholder="Your number" type="text" />
+        <input value={email} onChange={({ target }) => {setEmail(target.value)}} className={`bg-black outline-none rounded-md p-2 sm:w-3/12 border-2 ${emailError ? 'border-red-500' : 'border-fuchsia-400'}`} placeholder="Your email" type="text" />
         <input value="+ ADD" type="submit" className="bg-gray-700 rounded-md border-2 border-fuchsia-400 p-2 cursor-pointer transition duration-500 hover:bg-fuchsia-950 focus:bg-fuchsia-700"/>
     </form>
   )
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,7 +2,7 @@
 const Search = ({ onChange }) => {
   return (
     <div className="p-5">
-        <input type="text" className="outline-none rounded-md p-2 w-1/3 border-2 border-fuchsia-400" placeholder="Search your name here" onChange={onChange}/>
+        <input type="text" className="bg-black outline-none rounded-md p-2 w-1/3 border-2 border-fuchsia-400" placeholder="Search your name here" onChange={onChange}/>
     </div>
   )
 }
